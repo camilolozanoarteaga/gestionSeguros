7c9e2e34fd8e1febd4c1ad8b2a43ab45274b56ef
daniel.arboleda <daniel.arboleda@be-smart.co>
2018-11-16 12:53:31 -0500

    Se agrega la funcionalidad de ver las polizas creadas por un usuario

--- src/app/modulos/dashboard/clients/components/policies/policies.component.ts
+++ src/app/modulos/dashboard/clients/components/policies/policies.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedClientService } from '../../shared/shared-client.service';
 import { ClientInterface } from '@atention-models/client.interface';
+import { PolicyService } from '@atention-services/policy/policy.service';
+
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-policies',
@@ -9,10 +12,13 @@ import { ClientInterface } from '@atention-models/client.interface';
 })
 export class PoliciesComponent implements OnInit {
 
+
   client: ClientInterface;
+  policies: any;
 
   constructor(
     private _sharedClientService: SharedClientService,
+    private _policyService: PolicyService
   ) { }
 
   ngOnInit() {
@@ -20,8 +26,21 @@ export class PoliciesComponent implements OnInit {
     this._sharedClientService.clientCast$.subscribe((client) => {
 
       this.client = client;
+      this.getPolicies(this.client['id']);
 
     });
+
+  }
+
+  getPolicies(id: string) {
+    console.log(id)
+    this._policyService.getPolicies(id)
+      .subscribe((data) =>{
+
+        this.policies = data;
+        
+        console.log(this.policies[0])
+      });
 
   }
 
