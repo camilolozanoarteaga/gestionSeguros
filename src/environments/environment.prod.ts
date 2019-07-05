e06f59612e6bae25ed7661be6cfcc684a03d75a1
Camilo Lozano <camilolozanoarteaga@gmail.com>
2019-07-05 12:13:40 -0500

    Se sube a repo sin keys firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyCEjuDbgtbbjPKb0AQ_HMzBUDnacml78g4',
-    authDomain: 'client-crm-fc8e1.firebaseapp.com',
-    databaseURL: 'https://client-crm-fc8e1.firebaseio.com',
-    projectId: 'client-crm-fc8e1',
-    storageBucket: 'client-crm-fc8e1.appspot.com',
-    messagingSenderId: '1081026849519'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: 's'
   }
 };
